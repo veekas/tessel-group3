f98823d7c09901e4a0ed40e798d0b4d7296cc420
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-10-02 17:18:28 -0400

    updated package.json and tessel-server.js

--- tessel-server.js
+++ tessel-server.js
@@ -23,34 +23,36 @@ app.post('/pic', (req, res, next) => {
   });
 
   req.on('end', () => {
-    fs.writeFile('./perpShot.jpg', newPic);
-  });
+    fs.writeFile('./perpShot.jpg', newPic)
+      .then(function () {
+        // res.sendFile(__dirname + 'perpShot.jpg');
+        cloudinary.uploader.upload('perpShot.jpg', function (result) {
+          console.log(result.url)
+          axios.post('https://hooks.slack.com/services/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',
+            {
+              channel: '#loud-mouths',
+              username: 'Noise Police',
+              text: 'Keep your voices down!',
+              icon_emoji: ':police_car:',
+              attachments: [
+                {
+                  fallback: 'Today\'s perpetrators:',
+                  pretext: 'Today\'s perpetrators:',
+                  image_url: result.url
+                }
+              ]
+            })
+            .then(res => res.data)
+            .then(complete => {
+              console.log('complete: ', complete);
+              res.end()
+            });
+        })
+      })
+  })
+})
 
-});
+// app.get('/', (req, res, next) => {
 
-app.get('/pic', (req, res, next) => {
-  res.sendFile(__dirname + 'perpShot.jpg');
-  cloudinary.uploader.upload('perpShot.jpg', function (result) {
-    console.log(result.url)
-    axios.post('https://hooks.slack.com/services/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',
-      {
-        channel: '#loud-mouths',
-        username: 'Noise Police',
-        text: 'Keep your voices down!',
-        icon_emoji: ':police_car:',
-        attachments: [
-          {
-            fallback: 'Today\'s perpetrators:',
-            pretext: 'Today\'s perpetrators:',
-            image_url: result.url
-          }
-        ]
-      });
-    })
-    .then(res => res.data)
-    .then(complete => {
-      console.log('complete: ', complete);
-      res.send(complete);
-    });
-});
+// });
 
